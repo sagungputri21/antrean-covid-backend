40af957e6273fa8332b12d8cf78b337ced1608e3
Andhika Satrya <andhikangurah@gmail.com>
2021-12-16 15:33:32 +0800

    Mengubah metode hash pada route login menjadi fungsi hash yang dibuat sendiri (tidak menggunakan library)

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const hash = require('../src/hash');
 
 const Dokter = require('../models/dokter');
 const Pasien = require('../models/pasien');
@@ -12,12 +12,9 @@ router.post('/register', async (req, res) => {
   const { nama, nik, email, password, dokter } = req.body;
   const user = dokter ? new Dokter({ email, password, nama, nik }) : new Pasien({ email, password, nama, nik });
   try {
-    bcrypt.hash(user.password, parseInt(process.env.SALT_ROUNDS), async (err, hash) => {
-      if(err) console.log('Hashing error', err);
-      user.password = hash;
-      const newUser = await user.save();
-      res.status(201).json({ ok: true, data: newUser });
-    });
+    user.password = hash(password).toString();
+    const newUser = await user.save();
+    res.status(201).json({ ok: true, data: newUser });
   } catch (err) {
     res.status(400).json({ ok: false, message: err.message });
   }
@@ -30,7 +27,7 @@ router.post('/login', async (req, res) => {
   const user = dokter ? await Dokter.findOne({email}) : await Pasien.findOne({email});
   if(!user) return res.status(401).json({ok: false, message: 'Email or Password is incorrect!'});
   
-  const validated = await bcrypt.compare(password, user.password);
+  const validated = hash(password).toString() === user.password;
   if(!validated) return res.status(401).json({ok: false, message: 'Email or Password is incorrect!'});
   
   const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {expiresIn: '1h'});
